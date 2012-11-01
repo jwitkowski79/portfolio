7f14ca647c4c1c022958e359187fac55fc66c654
jen <jwitkowski79@gmail.com>
2012-11-01 16:54:22 -0400

    added wordpress files again

--- index.php
+++ index.php
@@ -1,17 +1,92 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define('WP_USE_THEMES', true);
+  <!-- End Header and Nav -->
 
-/** Loads the WordPress Environment and Template */
-require('./wp-blog-header.php');
+  <!-- Main Grid Section -->
+<?php get_header(); ?>
+
+  <div class="row">
+    <div class="six columns flush">
+      <div class="panel">
+        <h2>Recent Work</h2>
+      </div><!--/panel-->
+    </div><!--/col-->
+  </div><!--/row-->
+
+
+<div class="homeFeature">
+  <div class="row imageRow">
+    <div class="four columns flush">
+       <img src="images/home_1.jpg" />
+    </div><!--/col-->
+    <div class="four columns flush">
+     
+          <img src="images/home_2.jpg" />
+       
+    </div><!--/col-->
+    <div class="four columns flush">
+          <img src="images/home_3.jpg" />
+    </div><!--/col-->
+  </div><!--/row-->
+  <div class="row imageRow">
+    <div class="four columns flush">
+      <div class="panel">
+        <p>
+          <img src="images/home_4.jpg" />
+        </p>
+      </div><!--/panel-->
+    </div><!--/col-->
+    <div class="four columns flush">
+      <div class="panel">
+        <p>
+          <img src="images/home_5.jpg" />
+        </p>
+      </div><!--/panel-->
+    </div><!--/col-->
+    <div class="four columns flush">
+      <div class="panel">
+        <p>
+          <img src="images/home_6.jpg" />
+        </p>
+      </div><!--/panel-->
+    </div><!--/col-->
+  </div>
+</div><!--/homeFeature-->
+
+<div class="row">
+  <div class="six columns flush">
+    <div class="panel">
+        <h2>Blog</h2>
+    </div><!--/panel-->
+  </div><!--/col-->
+</div><!--/row-->
+
+<div class="row blogPreview">
+  <div class="four columns flush">
+      <div class="panel">
+        <h5>Panel Title</h5>
+        <p>This is a six columns grid panel with an arbitrary height. Bacon ipsum dolor sit amet salami ham hock biltong ball tip drumstick sirloin pancetta meatball short loin.</p>
+      </div><!--/panel-->
+    </div><!--/col-->
+    <div class="four columns flush">
+      <div class="panel">
+        <strong>Panel Title<strong>
+        <p>This is a three columns grid panel with an arbitrary height.</p>
+      </div>
+    </div>
+    <div class="four columns flush">
+      <div class="panel">
+        <h5>Panel Title</h5>
+        <p>This is a three columns grid panel with an arbitrary height.</p>
+      </div>
+    </div>
+
+
+ 
+
+  <!-- End Grid Section -->
+
+<?php get_header(); ?>
+
+  <!-- Footer -->
+
+ 
